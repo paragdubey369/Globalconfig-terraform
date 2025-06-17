d84691bdad9ebfbf25b7c3a5869f1962ceb6f284
s6568223@studenti.unige.it <paragdubey007@gmail.com>
2025-06-17 12:07:18 +0200

    globalconfig

---
+++ variable.tf
@@ -0,0 +1,31 @@
+# variable "environment_type" {
+#     type = string
+# }
+
+
+
+variable "control_nodes" {
+  type = map(any)
+  
+}
+
+variable "worker_nodes" {
+  type = map(any)
+  
+}
+
+variable "bastion_nodes" {
+  type = map(any)
+
+}
+
+
+variable "pm_api_url" {
+  type = string
+}
+variable "pm_api_token_id" {
+  type = string
+}
+variable "pm_api_token_secret" {
+  type = string
+}
