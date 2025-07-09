926f8ec3c6c841750bea4e14c781d7489e7802d4
s6568223@studenti.unige.it <paragdubey007@gmail.com>
2025-07-09 15:19:35 +0200

    g

--- environments/main/modules/vm/main.tf
+++ environments/main/modules/vm/main.tf
@@ -7,10 +7,13 @@ terraform {
   }
 }
 
+
 resource "proxmox_vm_qemu" "vm" {
   name        = var.vm_config.name
+  
   target_node = var.vm_config.node
   vmid        = var.vm_config.vm_id
+  
   
   memory      = var.vm_config.memory
   cpu {
@@ -22,30 +25,54 @@ resource "proxmox_vm_qemu" "vm" {
     size          = var.vm_config.disk_size
     type          = "disk"
     storage       = var.vm_config.storage
-    
     slot = "scsi0"
+    
   }
+  
+  disk { 
+    type = "cloudinit"
+    storage = var.vm_config.storage
+    slot = "scsi2"
+    
+    }
+    boot = "order=scsi0;scsi2;net0"
+    bios = "seabios"
+    scsihw = "virtio-scsi-single"
+    
+ 
 
   network {
     id = 0
     model  = "virtio"
     bridge = var.vm_config.bridge
+    firewall = true
   }
 
   clone      = var.vm_config.template
   full_clone = true
-  os_type    = "cloud-init"
+  os_type    = "126"
+  bootdisk   = "scsi0"
+  
+  
 
   ipconfig0  = "ip=${var.vm_config.ip}/24,gw=${var.vm_config.gateway}"
   nameserver = join(" ", var.vm_config.dns_servers)
 
-  ciuser  = "ubuntu"
+  ciuser  = "rhel"
   sshkeys = file("${path.module}/ssh.txt")
 
   
-  lifecycle {                                    # this is introduced so that proxmox do not modify anything on its own 
-    ignore_changes = [network, disk]
-  }
+
+  force_recreate_on_change_of = sha1(join("", [
+    var.vm_config.ip,
+    var.vm_config.name,
+    file("${path.module}/ssh.txt")
+  ]))
+
+  # lifecycle {                                    # this is introduced so that proxmox do not modify anything on its own 
+  #   ignore_changes = [network, disk]
+  # }
 
 }
+
 
