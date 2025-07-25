2b0704e48757e7aa10ae4c257d97d7710a60730f
s6568223@studenti.unige.it <paragdubey007@gmail.com>
2025-07-25 10:46:24 +0200

    ansible

--- environments/json_to_ini.py
+++ environments/json_to_ini.py
@@ -1,18 +1,24 @@
 import json
+import yaml
 import sys
 
-def json_to_ini(tf_output_file, ini_output_file):
+def json_to_yaml(tf_output_file, yaml_output_file):
     with open(tf_output_file, 'r') as f:
         data = json.load(f)
 
-    with open(ini_output_file, 'w') as f:
-        for group, content in data.items():
-            f.write(f"[{group}]\n")
-            for host, ip in content["value"].items():
-                f.write(f"{host} ansible_host={ip}\n")
-            f.write("\n")
+    inventory = {}
+
+    for group, content in data.items():
+        inventory[group] = {}
+        for host, ip in content["value"].items():
+            inventory[group][host] = {
+                "ansible_host": ip
+            }
+
+    with open(yaml_output_file, 'w') as f:
+        yaml.dump(inventory, f, sort_keys=False)
 
 if __name__ == "__main__":
     tf_output = sys.argv[1] if len(sys.argv) > 1 else "output.json"
-    ini_output = sys.argv[2] if len(sys.argv) > 2 else "inventory.ini"
-    json_to_ini(tf_output, ini_output)
+    yaml_output = sys.argv[2] if len(sys.argv) > 2 else "inventory.yml"
+    json_to_yaml(tf_output, yaml_output)
